9829e6eac1ddcfa85a4bb006ece34a6431de175c
Kavita Jakhar <kavita@antheminfotech.com>
2025-08-01 12:55:31 +0530

    added app js

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("✅ Backend is up and running!");
+});
+
 // API routes
 app.use('/api/roles', roleRoutes);
 
